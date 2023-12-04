dd1a6df8057f734c0b8d2016fd2cb45e7cf8f6b7
wnetohr <washingtonbdarocha@gmail.com>
2023-12-04 14:58:59 -0300

    font clarissans added to the project

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'uPlace',
       theme: ThemeData(
-        
+        fontFamily: 'clarissans',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
