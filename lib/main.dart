11bc348346f4d54c4ab4e24a17f2246cf2686dca
Christian Torres <christiantlira@gmail.com>
2023-12-11 19:25:29 -0300

    Criada conexão com o firestore

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,16 @@ import 'package:uplace/components/product_card.dart';
 import 'package:uplace/components/uplace_theme.dart';
 import 'package:uplace/frontend/colors.dart';
 import 'package:uplace/frontend/pages/homepage.dart';
+import 'package:firebase_core/firebase_core.dart'; //firebase
+import 'firebase_options.dart'; //firebase
 
-void main() {
+void main() async {
+  // adicionei o async por conta do await do firebase
+  WidgetsFlutterBinding
+      .ensureInitialized(); //essa linha é necessária para corrigir um erro do android
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -21,7 +29,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
-
