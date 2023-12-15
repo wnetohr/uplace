2a05b372e27752259f3a56b55a5fdf1bcc819919
wnetohr <washingtonbdarocha@gmail.com>
2023-12-15 13:04:03 -0300

    Sellers page creation

---
+++ lib/frontend/pages/sellerspage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:uplace/frontend/colors.dart';
+import 'package:uplace/frontend/routes/routes.dart';
+
+class SellersPage extends StatefulWidget {
+  const SellersPage({super.key});
+
+  @override
+  State<SellersPage> createState() => _SellersPageState();
+}
+
+class _SellersPageState extends State<SellersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
