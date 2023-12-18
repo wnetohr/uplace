750e8650ff418af3a381efec466d8961357731fc
wnetohr <washingtonbdarocha@gmail.com>
2023-12-18 14:29:23 -0300

    desenvolvimento da tela de vendedores

--- lib/frontend/pages/sellerspage.dart
+++ lib/frontend/pages/sellerspage.dart
@@ -18,24 +18,43 @@ class _SellersPageState extends State<SellersPage> {
         Expanded(
             flex: 1,
             child: Container(
-              color: Colors.black,
-            )),
-        Expanded(
-            flex: 3,
-            child: SingleChildScrollView(
+              color: Colors.white,
               child: Column(
                 children: [
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
+                  Expanded(
+                      flex: 3,
+                      child: Image.network(
+                          'https://images.pexels.com/photos/13330673/pexels-photo-13330673.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: AppColors.blueUplace,
+                            borderRadius: BorderRadius.circular(10)),
+                        margin: EdgeInsets.all(5),
+                      ))
                 ],
+              ),
+            )),
+        Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.white,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                  ],
+                ),
               ),
             ))
       ],
