7c652a0bedc5f80034369f833c5739fc3d14c6b0
wnetohr <washingtonbdarocha@gmail.com>
2024-01-15 17:47:28 -0300

    bug in register page fixed

--- lib/frontend/pages/registerpage.dart
+++ lib/frontend/pages/registerpage.dart
@@ -22,89 +22,91 @@ class _RegisterPageState extends State<RegisterPage> {
           style: TextStyle(color: AppColors.greenUplace),
         ),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text("Nome",style: TextStyle(fontSize: 18),),
-            const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: AppColors.greenUplace,
-                  border: OutlineInputBorder(),
-                  labelText: 'Insira seu nome'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text("E-mail",style: TextStyle(fontSize: 18),),
-            const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: AppColors.greenUplace,
-                  border: OutlineInputBorder(),
-                  labelText: 'Insira seu e-mail'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text("Senha",style: TextStyle(fontSize: 18),),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: AppColors.greenUplace,
-                  border: OutlineInputBorder(),
-                  labelText: 'Insira sua senha'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text("Confirme sua senha",style: TextStyle(fontSize: 18),),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: AppColors.greenUplace,
-                  border: OutlineInputBorder(),
-                  labelText: 'Insira sua senha novamente'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Container(
-                alignment: Alignment.center,
-                width: 220, // Ajuste a largura do botão conforme necessário
-                height: 60, // Ajuste a altura do botão conforme necessário
-                child: ElevatedButton(
-                  onPressed: () {
-                    RoutesFunctions.gotoHomePage(context);
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        AppColors.blueUplace), // Cor de fundo do botão
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40.0),
+      body: Center(
+        child: SingleChildScrollView(        
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Nome",style: TextStyle(fontSize: 18),),
+              const TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppColors.greenUplace,
+                    border: OutlineInputBorder(),
+                    labelText: 'Insira seu nome'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text("E-mail",style: TextStyle(fontSize: 18),),
+              const TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppColors.greenUplace,
+                    border: OutlineInputBorder(),
+                    labelText: 'Insira seu e-mail'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text("Senha",style: TextStyle(fontSize: 18),),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppColors.greenUplace,
+                    border: OutlineInputBorder(),
+                    labelText: 'Insira sua senha'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text("Confirme sua senha",style: TextStyle(fontSize: 18),),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppColors.greenUplace,
+                    border: OutlineInputBorder(),
+                    labelText: 'Insira sua senha novamente'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 220, // Ajuste a largura do botão conforme necessário
+                  height: 60, // Ajuste a altura do botão conforme necessário
+                  child: ElevatedButton(
+                    onPressed: () {
+                      RoutesFunctions.gotoHomePage(context);
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          AppColors.blueUplace), // Cor de fundo do botão
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40.0),
+                        ),
                       ),
                     ),
-                  ),
-                  child: const Text(
-                    'Criar conta',
-                    style: TextStyle(
-                      color: AppColors.lightblueUplace, // Cor do texto 'Entrar'
-                      fontSize: 28, // Ajusta o tamanho do texto
+                    child: const Text(
+                      'Criar conta',
+                      style: TextStyle(
+                        color: AppColors.lightblueUplace, // Cor do texto 'Entrar'
+                        fontSize: 28, // Ajusta o tamanho do texto
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
