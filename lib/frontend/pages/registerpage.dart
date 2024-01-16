c90f0cc43bc6b16ec3f43c0fe1251e05b085c3b2
wnetohr <washingtonbdarocha@gmail.com>
2024-01-16 13:15:13 -0300

    Email and Password verification and error alert added to register page

--- lib/frontend/pages/registerpage.dart
+++ lib/frontend/pages/registerpage.dart
@@ -14,7 +14,11 @@ class _RegisterPageState extends State<RegisterPage> {
   TextEditingController _userNameController = TextEditingController();
   TextEditingController _userEmailController = TextEditingController();
   TextEditingController _userPasswordController = TextEditingController();
-  TextEditingController _userConfirmPasswordController = TextEditingController();
+  TextEditingController _userConfirmPasswordController =
+      TextEditingController();
+  //Booleands for password and email verification
+  bool _isPasswordValid = true;
+  bool _isEmailValid = true;
 
   @override
   Widget build(BuildContext context) {
@@ -29,13 +33,16 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
       ),
       body: Center(
-        child: SingleChildScrollView(        
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text("Nome",style: TextStyle(fontSize: 18),),
+              const Text(
+                "Nome",
+                style: TextStyle(fontSize: 18),
+              ),
               TextField(
                 controller: _userNameController,
                 obscureText: false,
@@ -48,33 +55,65 @@ class _RegisterPageState extends State<RegisterPage> {
               const SizedBox(
                 height: 10,
               ),
-              const Text("E-mail",style: TextStyle(fontSize: 18),),
+              const Text(
+                "E-mail",
+                style: TextStyle(fontSize: 18),
+              ),
               TextField(
                 controller: _userEmailController,
                 obscureText: false,
-                decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: AppColors.greenUplace,
-                    border: OutlineInputBorder(),
-                    labelText: 'Insira seu e-mail'),
+                decoration: _isEmailValid
+                    ? const InputDecoration(
+                        filled: true,
+                        fillColor: AppColors.greenUplace,
+                        border: OutlineInputBorder(),
+                        labelText: 'Insira seu e-mail')
+                    : const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.red,
+                        border: OutlineInputBorder(),
+                        labelText: 'Digite um e-mail válido'),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  setState(() {
+                    _isEmailValid = _isValidEmail(value);
+                  });
+                },
               ),
               const SizedBox(
                 height: 10,
               ),
-              const Text("Senha",style: TextStyle(fontSize: 18),),
+              const Text(
+                "Senha",
+                style: TextStyle(fontSize: 18),
+              ),
               TextField(
                 controller: _userPasswordController,
                 obscureText: true,
-                decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: AppColors.greenUplace,
-                    border: OutlineInputBorder(),
-                    labelText: 'Insira sua senha'),
+                decoration: _isPasswordValid
+                    ? InputDecoration(
+                        filled: true,
+                        fillColor: AppColors.greenUplace,
+                        border: OutlineInputBorder(),
+                        labelText: 'Insira sua senha')
+                    : InputDecoration(
+                        filled: true,
+                        fillColor: Colors.red,
+                        border: OutlineInputBorder(),
+                        labelText: 'A senha deve ter mais de 8 digitos'),
+                onChanged: (value) {
+                  setState(() {
+                    _isPasswordValid = _isValidPassword(value);
+                  });
+                },
               ),
               const SizedBox(
                 height: 10,
               ),
-              const Text("Confirme sua senha",style: TextStyle(fontSize: 18),),
+              const Text(
+                "Confirme sua senha",
+                style: TextStyle(fontSize: 18),
+              ),
               TextField(
                 controller: _userConfirmPasswordController,
                 obscureText: true,
@@ -85,7 +124,7 @@ class _RegisterPageState extends State<RegisterPage> {
                     labelText: 'Insira sua senha novamente'),
               ),
               const SizedBox(
-                height: 20,
+                height: 60,
               ),
               Center(
                 child: Container(
@@ -95,15 +134,15 @@ class _RegisterPageState extends State<RegisterPage> {
                   child: ElevatedButton(
                     onPressed: () {
                       //Local para enviar o valor dos controllers para o banco de dados
-                      String nome = _userNameController.text;
-                      String email = _userEmailController.text;
                       String senha = _userPasswordController.text;
-                      String confirmarSenha = _userConfirmPasswordController.text;
-                      print('Nome: $nome');
-                      print('Email: $email');
-                      print('Senha: $senha');
-                      print('Confirmar senha: $confirmarSenha');
-                      RoutesFunctions.gotoHomePage(context);
+                      String confirmarSenha =
+                          _userConfirmPasswordController.text;
+                      if (_isEmailValid && _isPasswordValid && senha == confirmarSenha){
+                        RoutesFunctions.gotoHomePage(context);
+                      } else {
+                        _showAlertDialog(context);
+                      }
+                      //RoutesFunctions.gotoHomePage(context);
                     },
                     style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.all<Color>(
@@ -117,7 +156,8 @@ class _RegisterPageState extends State<RegisterPage> {
                     child: const Text(
                       'Criar conta',
                       style: TextStyle(
-                        color: AppColors.lightblueUplace, // Cor do texto 'Entrar'
+                        color:
+                            AppColors.lightblueUplace, // Cor do texto 'Entrar'
                         fontSize: 28, // Ajusta o tamanho do texto
                       ),
                     ),
@@ -128,6 +168,40 @@ class _RegisterPageState extends State<RegisterPage> {
           ),
         ),
       ),
+    );
+  }
+
+  bool _isValidEmail(String email) {
+    return email.contains('@') && email.contains('.');
+  }
+
+  bool _isValidPassword(String password) {
+    return password.length >= 8 &&
+        password.contains(RegExp(r'[a-zA-Z]')) &&
+        password.contains(RegExp(r'[0-9]'));
+  }
+  void _showAlertDialog(BuildContext context) {
+    // Cria o AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Erro no formulário!"),
+      content: Text("Verifique se as informações estão válidas."),
+      actions: [
+        // Adiciona botões ao alerta
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context); // Fecha o alerta
+          },
+          child: Text('Fechar'),
+        ),
+      ],
+    );
+
+    // Mostra o AlertDialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 }
