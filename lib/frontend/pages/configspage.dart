a6a7f703f339f62fc12385c7720bbbd11ae0bd79
Matheus Costa <bradventurer@gmail.com>
2024-01-16 19:51:03 -0300

    Create configspage
    it will come right after the users page, that will be created soon

---
+++ lib/frontend/pages/configspage.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:uplace/frontend/colors.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:uplace/frontend/routes/routes.dart';
+
