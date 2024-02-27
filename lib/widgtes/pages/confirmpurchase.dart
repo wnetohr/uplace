1070a3ec20f0c37a7e57320554b2c05c2d51ab68
wnetohr <washingtonbdarocha@gmail.com>
2024-02-27 20:44:49 -0300

    confirm purchase page fix

--- lib/widgtes/pages/confirmpurchase.dart
+++ lib/widgtes/pages/confirmpurchase.dart
@@ -32,27 +32,17 @@ class _ConfirmPurchasePageState extends State<ConfirmPurchasePage> {
                       Navigator.pop(context);
                     },
                   ),
-                  const SizedBox(width: 16),
-                  const CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://images.pexels.com/photos/247899/pexels-photo-247899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                  SizedBox(
+                    width: 30,
                   ),
-                  const SizedBox(width: 16),
                   const Text(
-                    'Doces do Jão',
+                    'Finalizando pedido',
                     style: TextStyle(
                       color: AppColors.greenUplace,
                     ),
                   ),
                 ],
-              ),
-              const Text(
-                'Pedido N° 1', // Mudará futuramente, só visual
-                style: TextStyle(
-                  fontSize: 16, // Tamanho menor do texto
-                  color: AppColors.greenUplace, // Cor do texto
-                ),
-              ),
+              )
             ],
           ),
         ),
@@ -60,41 +50,47 @@ class _ConfirmPurchasePageState extends State<ConfirmPurchasePage> {
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              Container(
-                width: double.infinity,
-                height: 200,
-                margin: const EdgeInsets.symmetric(vertical: 20),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.network(
-                    'https://fastly.4sqi.net/img/general/600x600/56867240_97Zuuj05YE1ws9gohnxS_mOhktrANOdFoIl1zgSZ1aY.jpg',
-                    fit: BoxFit.cover,
+              FractionallySizedBox(
+                widthFactor: 0.95,
+                child: Container(
+                  height: 200,
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      'https://fastly.4sqi.net/img/general/600x600/56867240_97Zuuj05YE1ws9gohnxS_mOhktrANOdFoIl1zgSZ1aY.jpg',
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: AppColors.blueUplace,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Local: CEAGRI 2',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+              FractionallySizedBox(
+                widthFactor: 0.90,
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: AppColors.blueUplace,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Local: CEAGRI 2',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Você está comprando:',
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
-                  ],
+                      SizedBox(height: 10),
+                      Text(
+                        'Você está comprando:',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(height: 10),
@@ -112,11 +108,13 @@ class _ConfirmPurchasePageState extends State<ConfirmPurchasePage> {
                   },
                 ),
               ),
-              const SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              const SizedBox(height: 30),
+              Flex(
+                direction: Axis.horizontal,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Expanded(
+                    flex: 1,
                     child: ElevatedButton(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
@@ -125,25 +123,33 @@ class _ConfirmPurchasePageState extends State<ConfirmPurchasePage> {
                           borderRadius: BorderRadius.circular(20),
                         ),
                       ),
-                      child: const Row(
+                      child: const Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Icon(
                             FontAwesomeIcons.whatsapp,
-                            size: 30,
+                            size: 20,
                             color: AppColors.greenUplace,
                           ),
-                          SizedBox(width: 8),
-                          Text(
-                            'WhatsApp',
-                            style: TextStyle(
-                                fontSize: 16, color: AppColors.greenUplace),
-                          ),
+                          SizedBox(width: 4),
+                          FittedBox(
+                            fit: BoxFit
+                                .scaleDown, // Redimensiona o texto para caber dentro do widget
+                            child: Text(
+                              'WhatsApp',
+                              style: TextStyle(
+                                  fontSize: 12, color: AppColors.greenUplace),
+                            ),
+                          )
                         ],
                       ),
                     ),
                   ),
+                  SizedBox(
+                    width: 6,
+                  ),
                   Expanded(
+                    flex: 1,
                     child: ElevatedButton(
                       onPressed: () {
                         RoutesFunctions.gotoChatPage(context);
@@ -154,25 +160,29 @@ class _ConfirmPurchasePageState extends State<ConfirmPurchasePage> {
                           borderRadius: BorderRadius.circular(20),
                         ),
                       ),
-                      child: const Row(
+                      child: const Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Icon(
                             Icons.message_rounded,
-                            size: 30,
+                            size: 20,
                             color: AppColors.greenUplace,
                           ),
-                          SizedBox(width: 8),
+                          SizedBox(width: 4),
                           Text(
                             'Chat',
                             style: TextStyle(
-                                fontSize: 16, color: AppColors.greenUplace),
+                                fontSize: 12, color: AppColors.greenUplace),
                           ),
                         ],
                       ),
                     ),
                   ),
+                  SizedBox(
+                    width: 6,
+                  ),
                   Expanded(
+                    flex: 1,
                     child: ElevatedButton(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
@@ -181,19 +191,19 @@ class _ConfirmPurchasePageState extends State<ConfirmPurchasePage> {
                           borderRadius: BorderRadius.circular(20),
                         ),
                       ),
-                      child: const Row(
+                      child: const Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Icon(
                             FontAwesomeIcons.telegram,
-                            size: 30,
+                            size: 20,
                             color: AppColors.greenUplace,
                           ),
-                          SizedBox(width: 8),
+                          SizedBox(width: 4),
                           Text(
                             'Telegram',
                             style: TextStyle(
-                                fontSize: 16, color: AppColors.greenUplace),
+                                fontSize: 12, color: AppColors.greenUplace),
                           ),
                         ],
                       ),
